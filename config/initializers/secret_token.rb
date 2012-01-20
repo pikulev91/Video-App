fab3a718df37856805f0b025a81e17ab15a2935d
pikulev91 <egor.pikulev@gmail.com>
2012-01-21 03:06:17 +0400

    Test commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Videoapp::Application.config.secret_token = 'a7611968650f0e2be8c6a121ebe465a29259dd00c0777b873416f15dac066affb2397c108ce034171386706ded7cd75c484f704db8ae0220eba36f36da648f8e'
